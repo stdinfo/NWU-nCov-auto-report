02b547131bb605176d37c85c1028b97014e78628
wzp100 <62051751+wzp100@users.noreply.github.com>
2021-01-17 19:35:40 +0800

    Update auto.py

--- auto.py
+++ auto.py
@@ -14,8 +14,8 @@ import argparse
 auth_mode = "PASSWORD"  #1:PASSWORD 2:COOKIES
 
 #统一身份认证账号密码，仅在“PASSWORD”认证模式下需要
-stu_id = ""
-stu_passwd = ""
+stu_id = "2019115072"
+stu_passwd = "wzpc201334104/"
 
 #app.nwu.edu.cn认证Cookies，仅在“COOKIES”认证模式下需要
 #可以通过浏览器直接获取
@@ -25,7 +25,7 @@ stu_varify_cookies = {
     }
 
 #调试信息开关
-debug_mode = False
+debug_mode = Ture
 
 #是否在账号密码登陆成功后输出app.nwu.edu.cn认证Cookies（即“COOKIES”认证模式下输入参数）
 is_print_cookies = False
@@ -99,7 +99,7 @@ def encrypt(text,key,iv):
 
 
 # Login functions
-def get_cookies(username='2015000001',password='123456abc'):
+def get_cookies(username='2019115072',password='wzpc201334104/'):
     #cookies_res = {}
 
     ncov_report_url = "https://app.nwu.edu.cn/site/ncov/dailyup"
@@ -210,13 +210,13 @@ def sent_report(cookies):
     headers = {"Accept":"application/json, text/plain, */*","Content-Type":"application/x-www-form-urlencoded","X-Requested-With":"XMLHttpRequest"}
     # cookies = {"UUkey":"","eai-sess":""}
     params = {
-        "sfzx":"1", #是否在校
+        "sfzx":"0", #是否在校
         "tw":"1",   #体温（list）(0-"Below 36";1-"36-36.5";2-"36.5-36.9";3-"36.9-37.3"; ... , i<=8)
-        "area":"陕西省 西安市 长安区",
-        "city":"西安市",
-        "province":"陕西省",
-        "address":"陕西省西安市长安区郭杜街道西北大学南校区学生公寓10号楼西北大学长安校区",
-        "geo_api_info":'{"type":"complete","info":"SUCCESS","status":1,"$Da":"jsonp_687452_","position":{"Q":34.14218,"R":108.87518999999998,"lng":108.87519,"lat":34.14218},"message":"Get ipLocation success.Get address success.","location_type":"ip","accuracy":null,"isConverted":true,"addressComponent":{"citycode":"029","adcode":"610116","businessAreas":[],"neighborhoodType":"","neighborhood":"","building":"","buildingType":"","street":"文苑南路","streetNumber":"11号","country":"中国","province":"陕西省","city":"西安市","district":"长安区","township":"郭杜街道"},"formattedAddress":"陕西省西安市长安区郭杜街道西北大学南校区学生公寓10号楼西北大学长安校区","roads":[],"crosses":[],"pois":[]}',   #高德SDK返回值
+        "area":"安徽省 宿州市 萧县",
+        "city":"宿州市",
+        "province":"安徽省",
+        "address":"安徽省宿州市萧县顺河路75",
+        "geo_api_info":'{"type":"complete","info":"SUCCESS","status":1,"$Da":"jsonp_687452_","position":{"Q":34.196683,"R":116.947843,"lng":116.947843,"lat":34.196683},"message":"Get ipLocation success.Get address success.","location_type":"ip","accuracy":null,"isConverted":true,"addressComponent":{"citycode":"0557","adcode":"341322","businessAreas":[],"neighborhoodType":"","neighborhood":"","building":"","buildingType":"","street":"","streetNumber":"","country":"中国","province":"安徽省","city":"宿州市","district":"萧县","township":""},"formattedAddress":"安徽省宿州市萧县顺河路75","roads":[],"crosses":[],"pois":[]}',   #高德SDK返回值
         "sfcyglq":"0",  #是否隔离期
         "sfyzz":"0",    #是否有症状
         "qtqk":"",  #其他情况
